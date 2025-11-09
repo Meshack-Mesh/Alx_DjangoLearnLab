ee7c324b65ef679c99ef3c512329aa21cfe30f0f
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-11-09 22:16:42 +0300

    Django Models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,5 +13,10 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('member-view/', views.member_view, name='member_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
+
+    # Custom permission secured views
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
   ]
 
