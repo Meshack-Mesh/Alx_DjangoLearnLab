a57bebdfdeb1f1330515109484f75dd0bae307d0
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-11-26 17:31:05 +0300

    Building Custom Views and Generic Views in Django REST Framework

---
+++ advanced-api-project/api/README.md
@@ -0,0 +1,15 @@
+### Filtering
+Use query parameters to filter results:
+- `/api/books/?title=Example`
+- `/api/books/?publication_year=2005`
+- `/api/books/?author=1`
+
+### Searching
+Search across specific fields:
+- `/api/books/?search=tolkien`
+- `/api/books/?search=magic`
+
+### Ordering
+Sort results:
+- `/api/books/?ordering=title`
+- `/api/books/?ordering=-publication_year`
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Book
 from .serializers import BookSerializer
 
@@ -44,3 +47,23 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminUser]
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Filtering, Searching, Ordering
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    # ---- FILTERING ----
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # ---- SEARCH ----
+    # search title or author's name
+    search_fields = ['title', 'author__name']
+
+    # ---- ORDERING ----
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # default ordering
