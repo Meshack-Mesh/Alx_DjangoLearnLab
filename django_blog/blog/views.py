0e947051c2f33903724fa9435fff17a13bbbc4cf
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-12-03 12:24:34 +0300

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -92,18 +92,24 @@ def profile_view(request):
 
     return render(request, "blog/profile.html", {"u_form": u_form, "p_form": p_form})
 
-@login_required
-def comment_create(request, post_pk):
-    post = get_object_or_404(Post, pk=post_pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect(post.get_absolute_url() + "#comment-{}".format(comment.pk))
-    return redirect(post.get_absolute_url())
+
+# ----- Comment Views -----
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        post_pk = self.kwargs.get('post_pk')
+        post = get_object_or_404(Post, pk=post_pk)
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url() + "#comment-{}".format(self.object.pk)
+
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
@@ -117,6 +123,7 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_success_url(self):
         return self.object.post.get_absolute_url() + "#comment-{}".format(self.object.pk)
 
+
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
@@ -129,42 +136,3 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.object.post.get_absolute_url()
 
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'   # blog/templates/blog/post_list.html
-    context_object_name = 'posts'
-    paginate_by = 10
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def test_func(self):
-        post = self.get_object()
-        return post.author == self.request.user
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('blog:post_list')
-
-    def test_func(self):
-        post = self.get_object()
-        return post.author == self.request.user
-    
-    
